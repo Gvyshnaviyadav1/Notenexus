96ff2aeda5f90de69ce6cb1a59547f61e0e00d02
Gvyshnaviyadav1 <vyshnavi.yadav.23031@iitgoa.ac.in>
2025-05-11 09:32:08 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,36 @@
+import socket
+import threading
+
+def listen_for_messages(sock):
+    while True:
+        try:
+            msg = sock.recv(1024).decode("utf-8", "replace")
+            print(msg, end="", flush=True)
+        except:
+            print("\nDisconnected from server.")
+            break
+
+def start_client(host='localhost', port=1206):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        sock.connect((host, port))
+    except ConnectionRefusedError:
+        print("Could not connect to server.")
+        return
+    #threading.Thread(target=listen_for_messages, args=(sock,), daemon=True).start()
+    listener = threading.Thread(target=listen_for_messages, args=(sock,))
+    listener.start()
+    while True:
+        try:
+            msg = input()
+            sock.sendall(msg.encode())
+            if msg.strip().isdigit() and int(msg.strip()) == 13:
+                print("Logging out.")
+                break
+        except:
+            print("Error sending message.")
+            break
+    sock.close()
+
+if __name__ == "__main__":
+    start_client()
